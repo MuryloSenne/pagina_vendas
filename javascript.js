233c4529b6a1ce093d5b4bd22a8f223fc5d3580f
MuryloSenne <muh.senne23@gmail.com>
2023-09-16 15:03:17 -0300

    Carousel

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const carousel = document.querySelector('.carousel');
+const cards = document.querySelectorAll('.card');
+let currentIndex = 0;
+
+function showCard(index) {
+  const offset = -index * cards[0].offsetWidth;
+  carousel.style.transform = `translateX(${offset}px)`;
+}
+
+function nextCard() {
+  currentIndex = (currentIndex + 1) % cards.length;
+  showCard(currentIndex);
+}
+
+function prevCard() {
+  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+  showCard(currentIndex);
+}
+
+setInterval(nextCard, 8000); // Alterna para o próximo card a cada 3 segundos
